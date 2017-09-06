aed41d238af41454feddeeafcb12bb00e972c36e
Tashaf <sardartashaf@gmail.com>
2017-09-06 10:19:11 +0500

    Initial commit

---
+++ app/src/main/java/com/tashaf/android/simplelistviewkotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tashaf.android.simplelistviewkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
